809b3a3e0a27009c63fa8f1a9d3722a105e0f0d8
Oliver Ferrigni <oliver@chef.io>
2015-09-24 15:16:45 -0500

    Initial generate

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'languages'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures languages'
+long_description 'Installs/Configures languages'
+version '0.1.0'
