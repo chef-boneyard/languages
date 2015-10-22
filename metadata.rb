74bc1fcf06a8713296b8f83ae0390d186ca3b2a7
Seth Chisamore <schisamo@chef.io>
2015-10-22 11:51:55 -0400

    Bump version to 0.1.8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'releng@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures languages'
 long_description 'Installs/Configures languages'
-version '0.1.7'
+version '0.1.8'
 
 depends 'chef-sugar', '~> 3.1'
 depends 'build-essential', '~> 2.2'
