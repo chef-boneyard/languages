b6b001445db84f724089e424eee32928fc6fbe81
Seth Chisamore <schisamo@chef.io>
2015-10-29 22:04:55 -0400

    Improve base `LanguageExecute` resource/provider classes
    These classes define the shared attributes and core action along with
    some abstract methods each provider must implement.

--- test/shared/spec_helper.rb
+++ test/shared/spec_helper.rb
@@ -12,3 +12,23 @@ end
 set :path, '/sbin:/usr/local/sbin:/usr/bin:/bin'
 
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |file| require_relative(file) }
+
+def windows?
+  os[:family] == 'windows'
+end
+
+def default_prefix_base
+  if windows?
+    'C:/languages'
+  else
+    '/opt/languages'
+  end
+end
+
+def chef_file_cache
+  if windows?
+    'C:/Users/vagrant/AppData/Local/Temp/kitchen/cache'
+  else
+    '/tmp/kitchen/cache'
+  end
+end
