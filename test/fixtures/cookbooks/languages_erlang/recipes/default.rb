3a2097b71f9169c2d3522127e8c97f2c5a25e419
Ryan Hass <rhass@users.noreply.github.com>
2017-02-10 16:56:22 -0800

    Refactor erlang_install to support latest kerl.
    There were a number of breaking changes in kerl over the past year. The
    biggest change is that the install directory must either be empty or
    not exist. As result, this is a breaking change in the cookbook as we
    can no longer install at top level directories such as `/usr/local`. A
    helper method was added to raise an error when the prefix path does not
    meet this requirement, before attempting build to prevent a long delay
    before identifying the issue.
    
    Additionally, kerl returns a non-zero status when a build request is
    made for an existing build. New logic was added to make the the build
    idempotent.
    
    Signed-off-by: Ryan Hass <rhass@users.noreply.github.com>

--- test/fixtures/cookbooks/languages_erlang/recipes/default.rb
+++ test/fixtures/cookbooks/languages_erlang/recipes/default.rb
@@ -2,15 +2,15 @@
 #########################################################################
 # Basic Install with Execution
 #########################################################################
-erlang_install '18.1'
+erlang_install '19.2'
 
 erlang_execute "erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell > /tmp/erlang_version" do
-  version '18.1'
+  version '19.2'
 end
 
 #########################################################################
 # Non-default Prefix
 #########################################################################
-erlang_install '18.0' do
-  prefix '/usr/local'
+erlang_install '19.1' do
+  prefix '/usr/local/erlang'
 end
